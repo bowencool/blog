c14996a80bb9f56ef4555bd64afaaed296614f06
Bowen <root@bowen.cool>
2024-01-09 23:39:37 +0800

    feat: getPostsByLang

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -1,18 +1,38 @@
 import { getCollection } from "astro:content";
 
+function getPostLang(postId: string) {
+  return postId.match(/^(\w+)\//)?.[1] || "en";
+}
 // export default function filterPosts(post) {third}
 export async function getPostsByLang(lang: string) {
   const allPosts = await getCollection("blog");
-  const filteredPosts = allPosts.filter(({ data, id }) => {
-    if (data.draft) return false;
-    if (lang === "en") {
-      return !id.match("\\w+/");
+  const map = new Map<string, any[]>();
+  const filteredPosts: typeof allPosts = [];
+  allPosts.forEach(({ data, id, slug }, index) => {
+    if (data.draft) return;
+    const { permalink } = data;
+    const key = permalink || slug;
+    // console.log(index, id, key, postLang);
+
+    if (!map.has(key)) {
+      map.set(key, [allPosts[index]]);
     } else {
-      console.log(lang, id, id.startsWith(`${lang}/`));
-      return id.startsWith(`${lang}/`);
+      const posts = map.get(key);
+      posts!.push(allPosts[index]);
     }
   });
-  // TODO 仅移除重复的文章，而不是移除所有非当前语言的文章
-  // console.log(filteredPosts.length);
+  // let i = 0;
+  map.forEach((posts, key) => {
+    // console.log(i++, key, posts.length);
+    const targetLangPost = posts.find(post => getPostLang(post.id) === lang);
+    if (targetLangPost) {
+      // console.log("\thit", targetLangPost.id);
+      filteredPosts.push(targetLangPost);
+    } else {
+      // console.log("\tmiss", posts[0].id);
+      filteredPosts.push(posts[0]);
+    }
+  });
+  console.log(lang, filteredPosts.length, allPosts.length);
   return filteredPosts;
 }
