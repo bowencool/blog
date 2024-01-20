8d02e7d1d1c48deb26b8ac00070c1dbdc41e8dd6
Bowen <root@bowen.cool>
2024-01-20 10:21:42 +0800

    fix favicon

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,8 +4,9 @@ import { SITE } from "@config";
 import { localizeUrl, localizePath } from "astro-i18next";
 import { getPostsByLang } from "@utils/getPosts";
 import i18next, { t, changeLanguage } from "i18next";
+import type { APIContext } from "astro";
 
-export async function GET() {
+export async function GET(ctx: APIContext) {
   changeLanguage("en");
   const posts = await getPostsByLang(i18next.language);
   const sortedPosts = getSortedPosts(posts);
@@ -13,11 +14,13 @@ export async function GET() {
     title: t("websiteTitle"),
     description: t("websiteDescription"),
     site: localizeUrl(SITE.website),
+    // customData: "<image></image>",
     items: sortedPosts.map(({ data, slug }) => ({
       link: localizePath(`/posts/${data.permalink || slug}`),
       title: data.title,
       description: data.description,
       pubDate: new Date(data.modDatetime ?? data.pubDatetime),
     })),
+    trailingSlash: true,
   });
 }
