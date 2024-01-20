57413e1e8eeec231ccb5816706c0edefe3533824
Bowen <root@bowen.cool>
2024-01-20 14:38:06 +0800

    fix i18n

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@utils/generateOgImages";
+import { changeLanguage } from "i18next";
 
-export const GET: APIRoute = async () =>
-  new Response(await generateOgImageForSite(), {
+export const GET: APIRoute = async () => {
+  changeLanguage("en");
+  return new Response(await generateOgImageForSite(), {
     headers: { "Content-Type": "image/png" },
   });
+};
--- src/pages/posts/[permalink]/index.png.ts
+++ src/pages/posts/[permalink]/index.png.ts
@@ -2,6 +2,7 @@ import type { APIRoute } from "astro";
 import { type CollectionEntry } from "astro:content";
 import { generateOgImageForPost } from "@utils/generateOgImages";
 import { getPostsByLang } from "@utils/getPosts";
+import { changeLanguage } from "i18next";
 
 export async function getStaticPaths() {
   const posts = await getPostsByLang("en").then(p =>
@@ -14,7 +15,12 @@ export async function getStaticPaths() {
   }));
 }
 
-export const GET: APIRoute = async ({ props }) =>
-  new Response(await generateOgImageForPost(props as CollectionEntry<"blog">), {
-    headers: { "Content-Type": "image/png" },
-  });
+export const GET: APIRoute = async ({ props }) => {
+  changeLanguage("en");
+  return new Response(
+    await generateOgImageForPost(props as CollectionEntry<"blog">),
+    {
+      headers: { "Content-Type": "image/png" },
+    }
+  );
+};
