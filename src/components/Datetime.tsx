3acbe689c6242935b0726bc98dcc58b8da0f698c
Bowen <root@bowen.cool>
2025-07-14 12:09:01 +0800

    fix timezone

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -1,4 +1,3 @@
-import i18next from "i18next";
 import { useTranslation } from "react-i18next";
 
 interface DatetimesProps {
@@ -12,31 +11,20 @@ interface Props {
   className?: string;
 }
 
-export default function Datetime({
-  pubDatetime,
-  modDatetime,
-  size = "sm",
-  className,
-}: Props) {
+export default function Datetime({ pubDatetime, modDatetime, size = "sm", className }: Props) {
   const { t } = useTranslation();
   return (
     <div className={`flex items-center space-x-2 opacity-80 ${className}`}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className={`${
-          size === "sm" ? "scale-90" : "scale-100"
-        } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
+        className={`${size === "sm" ? "scale-90" : "scale-100"} inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
         aria-hidden="true"
       >
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
         <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
       </svg>
 
-      <div
-        className={`flex w-full justify-between italic ${
-          size === "sm" ? "text-sm" : "text-base"
-        }`}
-      >
+      <div className={`flex w-full justify-between italic ${size === "sm" ? "text-sm" : "text-base"}`}>
         <span>
           <span className="sr-only">{t("publishedAt")} </span>
           <FormattedDatetime date={pubDatetime} />
@@ -53,19 +41,21 @@ export default function Datetime({
 }
 
 const FormattedDatetime = (props: DatetimesProps) => {
+  const { i18n } = useTranslation();
   const myDatetime = new Date(props.date);
 
-  const date = myDatetime.toLocaleDateString(i18next.language, {
+  const date = myDatetime.toLocaleDateString(i18n.language, {
     year: "numeric",
     month: "short",
     day: "numeric",
   });
 
-  const time = myDatetime.toLocaleTimeString(i18next.language, {
+  const time = myDatetime.toLocaleTimeString(i18n.language, {
     hour: "2-digit",
     minute: "2-digit",
   });
 
+  // throw new Error(`i18n.language: ${i18n.language}, date: ${date}, time: ${time}`);
   return (
     <>
       <time dateTime={myDatetime.toISOString()}>{date}</time>
